fc6fbc8144e8aef4ad61c4a270a90802ecf2225f
Josivaldo <josivaldojoao14@gmail.com>
2021-02-16 11:55:23 -0300

    Database First utilizando o Entity Framework

---
+++ Learning_DataBaseFirst/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Learning_DataBaseFirst.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
